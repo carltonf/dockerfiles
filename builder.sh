51820030df20d6d2d4f4cfb850d527f4b1de7205
Carl Xiong <xiongc05@gmail.com>
2016-08-10 18:39:15 +0800

    add builder script

---
+++ builder.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+NAMESPACE='carltonf'
+DATE_VERSION=$(date +%Y%m%d)
+
+img_name=$1
+if [ -z "$img_name" -a -d "$img_name" ];then
+    error "* Error: need a valid image name (subdir). Abort."
+    exit 1
+fi
+
+# NOTE set in label when build
+GIT_META=$(git log -s --format=%H -1 -- "$img_name")
+
+# NOTE: building in the subdir
+cd $img_name
+# NOTE sanitize image name for tags
+img_name=$(basename $img_name)
+
+# NOTE assemble tags
+base_tag="$NAMESPACE/$img_name"
+versioned_tag="${base_tag}:$DATE_VERSION"
+# NOTE for local convenience, mainly
+latest_tag="${base_tag}:latest"
+
+docker build --label="GIT_COMMIT=$GIT_META" \
+       --tag="$versioned_tag" \
+       .
+
+if [ $? -eq 0 ]; then
+    echo "* INFO: successfully built. tag it to latest."
+    docker tag "$versioned_tag" "$latest_tag"
+else
+    echo "* WARNING: Oops, something goes wrong. No latest tag set."
+fi
